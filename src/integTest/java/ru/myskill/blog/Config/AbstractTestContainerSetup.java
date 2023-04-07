5354b1a7b64c180429917b5c93c664267efbb9a4
Vitaly <woodanon123@gmail.com>
2023-04-07 15:31:14 +0500

    Добавление конфиг k8s, отладка конфига докера

--- src/integTest/java/ru/myskill/blog/Config/AbstractTestContainerSetup.java
+++ src/integTest/java/ru/myskill/blog/Config/AbstractTestContainerSetup.java
@@ -1,11 +1,14 @@
 package ru.myskill.blog.Config;
 
 import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.testcontainers.containers.ContainerState;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.File;
@@ -21,23 +24,13 @@ import java.io.IOException;
 @AllArgsConstructor
 public class AbstractTestContainerSetup {
 
-    private static DockerComposeContainer dockerComposeContainerStarter;
-    public static DockerComposeContainer dockerComposeContainer;
-
-    @BeforeAll
-    static void setUpDockerComposeContainers() throws IOException, ClassNotFoundException {
-
-        // запуск через компос, с ожидаением 200 от rest api
-        dockerComposeContainerStarter = new
-                DockerComposeContainer(new File("cicd/docker/docker-compose.yml"))
-                .withLocalCompose(true)
-                .withPull(false)
-                .withTailChildContainers(false)
-                .withServices("postgres_blog", "liquidbase", "blog")
-                .waitingFor("blog", Wait.forHttp("/user").forStatusCode(200));
-        dockerComposeContainer = dockerComposeContainerStarter;
-        dockerComposeContainer.start();
-    }
-
+    @Container
+    private static DockerComposeContainer dockerComposeContainer = new
+            DockerComposeContainer(new File("cicd/docker/docker-compose.yml"))
+            .withLocalCompose(true)
+            .withPull(false)
+            .withTailChildContainers(false)
+            .withServices("postgres_blog", "liquidbase_blog", "blog")
+            .waitingFor("blog", Wait.forHttp("/user").forStatusCode(200));
 
 }
